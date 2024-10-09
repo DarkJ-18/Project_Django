5c812983f21cdfa5903424809a702075fb9478b3
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-10-09 16:42:40 -0500

    Update PythonImportHelper-v2-Completion.json and models.py

--- App/tienda/admin.py
+++ App/tienda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#python3 manage.py createsuperuser
+
+from .models import *
+admin.site.register(Product)
--- App/tienda/models.py
+++ App/tienda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    cod = models.IntegerField(unique=True)
+    nombre = models.CharField(max_length=254)
+    precio = models.IntegerField()
+    
+    #makemigrations tienda => cambios al model 
+    
+    #migrate
+    
